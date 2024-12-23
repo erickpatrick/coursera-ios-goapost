4f01af8e55676707705f4ca5d02b2da619df970c
erick patrick <github@erickpatrick.net>
2024-12-24 00:30:53 +0100

    adds IBOutlet for tableview and IBAction for add goal button

--- goalpost/Controllers/GoalsVC.swift
+++ goalpost/Controllers/GoalsVC.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func addNewGoalWasPressed(_ sender: Any) {
+        print("button was pressed")
+    }
 }
 
