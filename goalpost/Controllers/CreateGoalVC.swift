c59e903ebb4fab5f544131fef1b4f9cc221a24a5
erick patrick <github@erickpatrick.net>
2024-12-25 16:41:55 +0100

    improves handling of text view area
    whenever customer taps text view it now gets blank and only allow
    going to next screen if it's not `nil`, blank, nor using the
    default "placeholder" value

--- goalpost/Controllers/CreateGoalVC.swift
+++ goalpost/Controllers/CreateGoalVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CreateGoalVC: UIViewController {
+class CreateGoalVC: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var goalLongType: UIButton!
     @IBOutlet weak var goalShortType: UIButton!
@@ -19,6 +19,7 @@ class CreateGoalVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        goalTitle.delegate = self
         nextButton.bindtoKeyboard()
         goalShortType.setSelectedColor()
         goalLongType.setDeselectedColor()
@@ -42,6 +43,20 @@ class CreateGoalVC: UIViewController {
     }
     
     @IBAction func onNextPressed(_ sender: Any) {
+        if goalTitle.text != nil && goalTitle.text != "What is your goal?" && goalTitle.text != "" {
+            guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "FinishGoalVC") as? FinishGoalVC else {
+                return
+            }
+            
+            finishGoalVC.initData(description: goalTitle.text, type: goalType)
+            finishGoalVC.modalPresentationStyle = .overCurrentContext
+            
+            presentDetail(finishGoalVC)
+        }
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
     }
 
     /*
